1eef9c939e251bc335ff2a8761c856d8a8ef3f64
pavantammineedi <105170865+pavantammineedi@users.noreply.github.com>
2022-05-08 19:28:29 -0400

    Create index.php

---
+++ WAMP www/index.php
@@ -0,0 +1,47 @@
+
+<html>
+<style>
+
+.team{
+    margin-top: 16px;
+    text-align: center;
+    font-size: x-large;
+}
+.center {
+  margin: auto;
+  width: 50%; 
+  padding: 50px;
+
+}
+</style>
+  <head>
+    <title>Home Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">    
+  </head>
+  <body>
+   <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid" style="padding">        
+        <a class="navbar-brand" href="index.php"><i class="bi bi-house" style = "font-size: x-large;"></i></a>
+        </div>     
+        <p class="team">Team Hydra</li></p>
+        </div>
+        </nav>        
+          <div class="bg-image" style="background-image:url('img-bg.png'); height: 120vh;  background-repeat: no-repeat; background-size:cover;  ">
+            <div style="display: flex; justify-content:space-evenly;padding:5%;">
+         
+                <div>
+                  <a href="viewdbindex.php" class="btn btn-danger" style = "font-size: xxx-large;">View Results</a>
+                </div>
+                <div>
+                  <a href="adddataindex.php" class="btn btn-info" style = "font-size: xxx-large;">Add Data</a>
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
